48361f61553a69cd96bb7c320e40939ba6f5533a
islender <itheslayerman@gmail.com>
2022-10-22 10:01:18 +0100

    fixed ugly stuff

--- converter.py
+++ converter.py
@@ -1,7 +1,8 @@
 import os
 
-filename = input("Please choose the input file name (must be in same directory as script. Please include the .txt\n")
+filename = input("Please choose the input file name (must be in same directory as script. Please include the .txt)\n")
 proxtype = input("What type of proxy file is this? (socks4, socks5, https)?\n")
+outputfile = input("Name the output file (.txt not necessary)\n")
 
 with open(filename, "r+") as file:
     line = file.readline()
@@ -16,12 +17,12 @@ with open(filename, "r+") as file:
     while line:
         line = proxtype + " 	 " + line.replace(":", " ")
 
-        with open('newproxies' + '.txt', 'a') as out:
+        with open(outputfile + '.txt', 'a') as out:
             out.write(line)
             print(line)
 
         line = file.readline()
 
 print("-----------------------------------------")
-print("FINISHED! Find new file in newproxies.txt")
+print("FINISHED! Find new file in " + outputfile +".txt")
 print("-----------------------------------------")
